66fa8d49273b012a8aa1f6769f79e5ec765bb7f9
Võ Quốc <quoc17072006@gmail.com>
2021-07-24 06:08:01 +0000

    Commit #46

--- next.config.js
+++ next.config.js
@@ -11,20 +11,11 @@ module.exports = withBundleAnalyzer({
 		modern: true,
 	},
 
-	webpack: (config, options) => {
-		config.module.rules.push({
-			test: /\.(svg|png|jpe?g|gif|mp4)$/i,
-			use: [
-				{
-					loader: 'file-loader',
-					options: {
-						publicPath: '/_next',
-						name: 'static/media/[name].[hash].[ext]',
-					},
-				},
-			],
-		})
+	images: {
+		loader: 'imgix',
+	},
 
+	webpack: (config, options) => {
 		const mdx = [
 			options.defaultLoaders.babel,
 			{
@@ -35,49 +26,63 @@ module.exports = withBundleAnalyzer({
 			},
 		]
 
-		config.module.rules.push({
-			test: /\.mdx$/,
-			oneOf: [
-				{
-					resourceQuery: /preview/,
-					use: [
-						...mdx,
-						createLoader(function(src) {
-							if (src.includes('<!--more-->')) {
-								const [preview] = src.split('<!--more-->')
-								return this.callback(null, preview)
-							}
+		config.module.rules.push(
+			{
+				test: /\.(svg|png|jpe?g|gif|webp|mp4|webm)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							publicPath: '/_next',
+							name: 'static/media/[name].[hash].[ext]',
+						},
+					},
+				],
+			},
+			{
+				test: /\.mdx$/,
+				oneOf: [
+					{
+						resourceQuery: /preview/,
+						use: [
+							...mdx,
+							createLoader(function(src) {
+								if (src.includes('<!--more-->')) {
+									const [preview] = src.split('<!--more-->')
+									return this.callback(null, preview)
+								}
 
-							const [preview] = src.split('<!--/excerpt-->')
-							return this.callback(null, preview.replace('<!--excerpt-->', ''))
-						}),
-					],
-				},
-				{
-					resourceQuery: /rss/,
-					use: mdx,
-				},
-				{
-					use: [
-						...mdx,
-						createLoader(function(src) {
-							const content = [
-								'import Post from "@/components/Post"',
-								'export { getStaticProps } from "@/getStaticProps"',
-								src,
-								'export default Post',
-							].join('\n')
+								const [preview] = src.split('<!--/excerpt-->')
+								return this.callback(null, preview.replace('<!--excerpt-->', ''))
+							}),
+						],
+					},
+					{
+						resourceQuery: /rss/,
+						use: mdx,
+					},
+					{
+						use: [
+							...mdx,
+							createLoader(function(src) {
+								const content = [
+									'import Post from "@/components/Post"',
+									'export { getStaticProps } from "@/getStaticProps"',
+									src,
+									'export default Post',
+								].join('\n')
 
-							if (content.includes('<!--more-->')) {
-								return this.callback(null, content.split('<!--more-->').join('\n'))
-							}
+								if (content.includes('<!--more-->')) {
+									return this.callback(null, content.split('<!--more-->').join('\n'))
+								}
 
-							return this.callback(null, content.replace(/<!--excerpt-->.*<!--\/excerpt-->/s, ''))
-						}),
-					],
-				},
-			],
-		})
+								return this.callback(null, content.replace(/<!--excerpt-->.*<!--\/excerpt-->/s, ''))
+							}),
+						],
+					},
+				],
+			}
+		)
 
 		if (!options.dev && options.isServer) {
 			const originalEntry = config.entry
