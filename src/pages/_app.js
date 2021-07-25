4e2a035f78eb3cc0d7033181f0782c45b1904355
Võ Quốc <quoc17072006@gmail.com>
2021-07-25 14:44:22 +0000

    Commit #54

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -45,24 +45,6 @@ export default function App({ Component, pageProps }) {
                     color="#5bbad5"
                 />
                 <link rel="manifest" href="/manifest.json" />
-                <link
-                    rel="alternate"
-                    type="application/rss+xml"
-                    title="RSS 2.0"
-                    href="/feed.xml"
-                />
-                <link
-                    rel="alternate"
-                    type="application/atom+xml"
-                    title="Atom 1.0"
-                    href="/atom.xml"
-                />
-                <link
-                    rel="alternate"
-                    type="application/json"
-                    title="JSON Feed"
-                    href="/feed.json"
-                />
             </Head>
             <Component {...pageProps} />
         </div>
